cc1e3f573b907045ad96fe6359cce1171000b650
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-12-09 22:28:54 +0530

    Updated Remote images configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["img.icons8.com"] },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "img.icons8.com" }],
+  },
 };
 
 module.exports = nextConfig;
